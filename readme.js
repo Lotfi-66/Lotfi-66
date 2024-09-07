7634406a3214b930678f82c4f539c5ad1da00eb1
Lotfi Djeghbala <lotfi.djeg@gmail.com>
2024-09-08 00:33:43 +0200

    Update readme.js

--- readme.js
+++ readme.js
@@ -44,6 +44,14 @@ module.exports = `<h1 align="center">Hi 👋, I'm Lotfi</h1>
 </p>
 
 <p align="center">
+<picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://cdn.jsdelivr.net/gh/sun0225SUN/sun0225SUN/profile-snake-contrib/github-contribution-grid-snake-dark.svg" />
+    <source media="(prefers-color-scheme: light)" srcset="https://cdn.jsdelivr.net/gh/sun0225SUN/sun0225SUN/profile-snake-contrib/github-contribution-grid-snake.svg" />
+    <img alt="github-snake" src="https://cdn.jsdelivr.net/gh/sun0225SUN/sun0225SUN/profile-snake-contrib/github-contribution-grid-snake-dark.svg" />
+</picture>
+</p>
+
+<p align="center">
 Last update on <#today_date>
 </p>
 
